fb9a2c7ed347c368f15b39e8f422d6b363435d57
Jnorman2123 <Jnorman2123@gmail.com>
2020-01-14 20:54:44 -0500

    fixed error in omniauth route

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,5 @@
 class CallbacksController < Devise::OmniauthCallbacksController 
     def github 
-        binding.pry
         @user = User.from_omniauth(request.env["omniauth.auth"])
 
         sign_in_and_redirect root_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {registrations: 'registrations'}
+  devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
   devise_scope :user do 
     get 'signin', to: 'devise/sessions#new' 
-    get 'signup', to: 'registrations#new' 
+    get 'signup', to: 'devise/registrations#new' 
     delete 'logout', to: 'devise/sessions#destroy'
   end 
-  get '/users/auth/github/callback', to: 'devise/sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'application#welcome'
 end
