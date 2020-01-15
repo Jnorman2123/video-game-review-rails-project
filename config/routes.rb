2bd444cfc0dc359c694f2b83784b8215afc5a01d
Jnorman2123 <Jnorman2123@gmail.com>
2020-01-15 10:12:32 -0500

    added routes for games

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show]
+  resources :games
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
   devise_scope :user do 
     get 'signin', to: 'devise/sessions#new' 
