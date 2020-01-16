d7b9d822422bfc67f154aa696519729049adace6
Jnorman2123 <Jnorman2123@gmail.com>
2020-01-15 22:24:26 -0500

    created custome accepted attributes for console

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,18 @@
 class Game < ApplicationRecord
     has_many :reviews 
-    has_many :consoles 
+    has_and_belongs_to_many :consoles 
     has_many :users, through: :reviews 
     accepts_nested_attributes_for :consoles
     validates :title, presence: true, uniqueness: true 
+
+    def consoles_attributes=(consoles) 
+        consoles.each do |key, value|
+            value.each do |k,v|
+                console = Console.find_or_create_by(name: v) if !v.nil?
+                self.consoles << console
+                self.save
+            end 
+        end 
+    end 
     
 end
