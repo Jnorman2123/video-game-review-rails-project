4bb7c61612632a45654398d5e0c7dd338ff17612
Jnorman2123 <Jnorman2123@gmail.com>
2020-01-15 23:24:06 -0500

    created new form for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
     belongs_to :user 
     belongs_to :game 
-    validates :content, presence: true
+    validates :content, presence: true, length: {maximum: 250}
+    validates :rating, presence: true, inclusion: {in: %w(1 2 3 4 5 6 7 8 9 10)}
 end
