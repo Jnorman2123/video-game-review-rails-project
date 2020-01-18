8b04f90e64fea3e963a44bd51886c96224254699
Jnorman2123 <Jnorman2123@gmail.com>
2020-01-17 22:52:06 -0500

    refactored reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController 
-
+    before_action :set_review_or_invalid, only: [:show, :edit, :update, :destroy]
     def index 
         @reviews = Review.all
     end 
@@ -10,15 +10,18 @@ class ReviewsController < ApplicationController
 
     def create 
         @review = Review.create(review_params)
-        redirect_to game_path(@review.game_id)
+        if @review.save
+            redirect_to game_path(@review.game_id)
+        else 
+            review_errors
+            redirect_to game_path(@review.game_id)
+        end 
     end 
 
     def show 
-        @review = Review.find(params[:id])
     end 
 
     def edit 
-        @review = Review.find(params[:id])
         if !current_user.admin || current_user != @review.user
             flash[:notice] = "Only Admins or the Owner of the review can edit it."
             redirect_to game_review_path(@review)
@@ -26,16 +29,15 @@ class ReviewsController < ApplicationController
     end 
 
     def update 
-        @review = Review.find(params[:id])
         if @review.update(review_params)
             redirect_to game_path(@review.game_id)
         else 
-            redirect_to game_review_edit_path(@review)
+            review_errors 
+            redirect_to edit_game_review_path(@review)
         end
     end 
     
     def destroy 
-        @review = Review.find(params[:id])
         @review.delete 
         redirect_to game_path(@review.game)
     end 
@@ -45,4 +47,24 @@ class ReviewsController < ApplicationController
     def review_params 
         params.require(:review).permit(:content, :rating, :game_id, :user_id)
     end 
+
+    def set_review
+        Review.find_by_id(params[:id])
+    end 
+
+    def invalid_review
+        flash[:notice] = "That review does not exist."
+        redirect_to game_reviews_path 
+    end 
+
+    def set_review_or_invalid 
+        @review = set_review ? @review = set_review : invalid_review
+    end 
+
+    def review_errors 
+        flash[:notice] = "#{@review.errors.full_messages.first}"
+    end 
 end 
+
+
+
