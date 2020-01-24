69c5f09393d993a8b4de6644815a9641b12175a6
Jnorman2123 <Jnorman2123@gmail.com>
2020-01-24 11:46:52 -0500

    added admin checkbox to user sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,11 @@ class RegistrationsController < Devise::RegistrationsController
     private 
 
     def sign_up_params 
-        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+        params.require(:user).permit(:username, :email, :password, :password_confirmation, :admin)
     end 
 
     def account_update_params 
-        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+        params.require(:user).permit(:username, :email, :password, :password_confirmation, :admin)
     end 
 
 end 
